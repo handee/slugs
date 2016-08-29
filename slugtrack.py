bacc34b63784ce177c204510a97a5eaa860f6d91
hannah d <hmd@hannahdee.eu>
2016-08-29 19:47:04 +0100

    tidied up code a bit

--- slugtrack.py
+++ slugtrack.py
@@ -6,7 +6,15 @@ import video
 import math
 import numpy as np
 
-
+ 
+# edit this line to point to the right directory of jpgs
+#inputdir="/dcs/hmd1/data/lizzie/"
+#inputdir="/home/hmd1/dcshome2/data/lizzie/"
+inputdir="/home/hannah/Videos/lizzieslug/"
+frames_of_background=15 # magic number: how many frames do we have in our
+                        # moving average background model?
+difference_thresh=30 # magic number: how different does the background
+              #have to be before it counts as background?
 ###
 #
 # a function to prune the outputs of connected components, getting
@@ -31,14 +39,32 @@ def prune_outputs(o):
                 newstats.append(stats[i])            
 
     return(num_labels, newcentroids, newstats)
-  
-# edit this line to point to the right directory of jpgs
-#inputdir="/dcs/hmd1/data/lizzie/"
-#inputdir="/home/hmd1/dcshome2/data/lizzie/"
-inputdir="/home/hannah/Videos/lizzieslug/"
-frames_of_background=15 # magic number: how many frames do we have in our
-                        # moving average background model?
+ 
+###
+#
+# a function to prune the outputs of connected components, getting
+# rid of those which are un-slug-like because they are too big 
+#
+###
+def prune_outputs(o):
+    # The first cell is the number of labels
+    num_labels = o[0]
+    # The second cell is the label matrix
+    labels = o[1]
+    # The third cell is the stat matrix
+    stats = o[2]
+    # The fourth cell is the centroid matrix
+    centroids = o[3]
+    newcentroids=[]
+    newstats=[]
+    for i in range(0, num_labels):
+        if not (math.isnan(centroids[i][0])):
+            if (stats[i][4]<2000):
+                newcentroids.append(centroids[i])
+                newstats.append(stats[i])            
 
+    return(num_labels, newcentroids, newstats)
+ 
 # read directory, get list of files, sort
 flist=glob.glob(inputdir+"*.jpg")
 flist.sort()
@@ -48,13 +74,6 @@ cv2.namedWindow('foregound')
 cv2.namedWindow('input')
 cv2.namedWindow('slug')
 
-# trackbar related function
-def nothing(*arg):
-    pass
-
-#cv2.createTrackbar('Size of buffer', 'foreground', frames_of_background, 250, nothing)
-#cv2.createTrackbar('Difference threshold', 'foreground', 30, 200, nothing)
-
 n=0
 img=cv2.imread(flist[0])
 movingaverage=np.float32(img)
@@ -62,8 +81,6 @@ movingaverage=np.float32(img)
 for fname in flist:
 #read a frame from the video capture obj 
     frame=cv2.imread(fname)
-#check the trackbars to see if the default background model length has changed
-    #fbuffer=cv2.getTrackbarPos('Size of buffer', 'foreground')
     fbuffer=frames_of_background
 #let's deal with that pesky zero case before we divide by fbuffer
     if fbuffer==0:
@@ -81,9 +98,6 @@ for fname in flist:
     difference_img = cv2.absdiff(res, img_blur)
 # make that greyscale
     grey_difference_img = cv2.cvtColor(difference_img, cv2.COLOR_BGR2GRAY)
-# threshold this to find foreground objects
-    #difference_thresh=cv2.getTrackbarPos('Difference threshold', 'foreground')
-    difference_thresh=30
 # threshold it to get a motion mask
     ret,th1 = cv2.threshold(grey_difference_img,difference_thresh,255,cv2.THRESH_BINARY)
 # if we've had enough frames of background then our motion estimate is probably stable...
